aacc83a7692387be643a124f3756a1c855877dc3
Valentino Castro <jordano7gt@gmail.com>
2025-07-31 01:17:29 -0500

    Primer commit

---
+++ src/main/java/reservaCanchasDeportivas/rcd/RcdApplication.java
@@ -0,0 +1,13 @@
+package reservaCanchasDeportivas.rcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reservaCanchasDeportivas/rcd/RcdApplicationTests.java
@@ -0,0 +1,13 @@
+package reservaCanchasDeportivas.rcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
