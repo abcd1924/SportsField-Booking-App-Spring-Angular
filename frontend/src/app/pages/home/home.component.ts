c86a8ab3d1e30f92a05528a0437ae3e3d062f241
Valentino Castro <jordano7gt@gmail.com>
2025-09-10 23:50:27 -0500

    Se modificaron los componentes de home para mejorarlo estéticamente. Se añadió una imagen para el Hero section.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CardModule, DividerModule, TagModule, RouterLink, StyleClassModule], 
   providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss' 
